04ef96ae9886d4c9a9a3878be17b6cb5c24dd14c
stefano durante <duranteorama@gmail.com>
2019-12-04 21:43:09 +0100

    Lab08 with parcel

---
+++ 08_playground/avatar-business.js
@@ -0,0 +1,5 @@
+function getAvatar(user) {
+    const img = document.createElement("img");
+    img.src = user.avatar;
+    return img;
+}
---
+++ 08_playground/data-business.js
@@ -0,0 +1,108 @@
+function getUsers() {
+    return [{
+            id: 1,
+            avatar: "https://robohash.org/voluptatemdelenitinostrum.jpg?size=50x50&set=set1",
+            first_name: "Willem",
+            last_name: "Waterfall",
+            email: "wwaterfall0@t-online.de"
+        },
+        {
+            id: 2,
+            avatar: "https://robohash.org/aperiametlaboriosam.jpg?size=50x50&set=set1",
+            first_name: "Franni",
+            last_name: "Kemsley",
+            email: "fkemsley1@cloudflare.com"
+        },
+        {
+            id: 3,
+            avatar: "https://robohash.org/numquamexquidem.jpg?size=50x50&set=set1",
+            first_name: "Emmy",
+            last_name: "Eales",
+            email: "eeales2@chicagotribune.com"
+        },
+        {
+            id: 4,
+            avatar: "https://robohash.org/voluptatesdelectusducimus.jpg?size=50x50&set=set1",
+            first_name: "Carissa",
+            last_name: "Hubball",
+            email: "chubball3@github.com"
+        },
+        {
+            id: 5,
+            avatar: "https://robohash.org/auteumtotam.jpg?size=50x50&set=set1",
+            first_name: "Giff",
+            last_name: "Walmsley",
+            email: "gwalmsley4@npr.org"
+        },
+        {
+            id: 6,
+            avatar: "https://robohash.org/eumdoloremrepudiandae.jpg?size=50x50&set=set1",
+            first_name: "Star",
+            last_name: "Austick",
+            email: "saustick5@t-online.de"
+        },
+        {
+            id: 7,
+            avatar: "https://robohash.org/voluptatemetaut.jpg?size=50x50&set=set1",
+            first_name: "Flynn",
+            last_name: "Trask",
+            email: "ftrask6@live.com"
+        },
+        {
+            id: 8,
+            avatar: "https://robohash.org/aliasprovidentet.jpg?size=50x50&set=set1",
+            first_name: "Jill",
+            last_name: "Potier",
+            email: "jpotier7@baidu.com"
+        },
+        {
+            id: 9,
+            avatar: "https://robohash.org/odioestminima.jpg?size=50x50&set=set1",
+            first_name: "Joaquin",
+            last_name: "Paniman",
+            email: "jpaniman8@weibo.com"
+        },
+        {
+            id: 10,
+            avatar: "https://robohash.org/sequiinventorenemo.jpg?size=50x50&set=set1",
+            first_name: "Marion",
+            last_name: "Wetter",
+            email: "mwetter9@pbs.org"
+        },
+        {
+            id: 11,
+            avatar: "https://robohash.org/laborumeveniettenetur.jpg?size=50x50&set=set1",
+            first_name: "Tracey",
+            last_name: "Watmough",
+            email: "twatmougha@tripadvisor.com"
+        },
+        {
+            id: 12,
+            avatar: "https://robohash.org/voluptatemiureitaque.jpg?size=50x50&set=set1",
+            first_name: "Ardine",
+            last_name: "Westoff",
+            email: "awestoffb@auda.org.au"
+        },
+        {
+            id: 13,
+            avatar: "https://robohash.org/harumquibusdamsit.jpg?size=50x50&set=set1",
+            first_name: "Yoshi",
+            last_name: "Rennox",
+            email: "yrennoxc@moonfruit.com"
+        },
+        {
+            id: 14,
+            avatar: "https://robohash.org/verorecusandaedistinctio.jpg?size=50x50&set=set1",
+            first_name: "Sadella",
+            last_name: "Jex",
+            email: "sjexd@topsy.com"
+        },
+        {
+            id: 15,
+            avatar: "https://robohash.org/veroaliquamratione.jpg?size=50x50&set=set1",
+            first_name: "Rozanna",
+            last_name: "Mooring",
+            email: "rmooringe@ebay.co.uk"
+        }
+    ];
+}
