109091a139229bd14b249d306444e01201e8e6bc
stefano durante <duranteorama@gmail.com>
2019-12-12 21:35:44 +0100

    Lab09

--- 09_playground/src/index.js
+++ 09_playground/src/index.js
@@ -12,16 +12,16 @@
 
 
 // ahora lo hacemos con Axios
-// import axios from 'axios'
+import axios from 'axios'
 
-// axios.get("https://api.github.com/orgs/apple/members")
-//         return response.data
-//     })
-//     .then(data => {
-//         for (let item of data) {
-//             document.write(item.login)
-//         }
-//     })
+axios.get("https://api.github.com/orgs/apple/members").then(response => {
+        return response.data
+    })
+    .then(data => {
+        for (let item of data) {
+            document.write(item.login)
+        }
+    })
 
 // Practica
 //https: //codesandbox.io/s/bootcamp-lemoncode-mod8-practica-4-lblkf
